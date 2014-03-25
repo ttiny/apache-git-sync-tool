4eb8570ed5e5d98b5a9a3fc66d28c9c3e7829a71
Borislav Peev <borislav.asdf@gmail.com>
2014-03-25 16:37:00 +0200

    fix for "git fetch -all" not fetching all

--- sync.php
+++ sync.php
@@ -277,7 +277,7 @@
 				}
 				
 				// Pull
-				$branchcmd = $branchName != '*' ? 'origin '.$branchName : '"+refs/heads/*:refs/heads/*"';
+				$branchcmd = $branchName != '*' ? 'origin '.$branchName : 'origin "+refs/heads/*:refs/heads/*"';
 				$pull = $branchConfig->bare ? 'fetch' : 'pull -s recursive -X theirs';
 				$command = 'git '.$pull.' '.$branchcmd;
 				$returnCode = _executeCommand('Pulling', $command, $config->retryOnErrorCount, '_cleanUntrackedStuff', $branchConfig );
